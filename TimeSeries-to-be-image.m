8c4e42e629dfafe0aff4f4876d7b8c4d180d21d8
Apdullah <apdullahyayik@gmail.com>
2018-05-10 15:01:58 +0300

    Update TimeSeries-to-be-image.m

--- TimeSeries-to-be-image.m
+++ TimeSeries-to-be-image.m
@@ -1,16 +1,20 @@
 function im=tstbi(X, n, level, showP)
-%TIME SERIES-TO-BE-AN-IMAGE
-% X...............time series
-% n...............degree vector (first :R, second: G, third :B)
-% level...........size of grid matrix (level x level)
-% showP...........show image option (1 show, 0 not)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%TSTBI    Transforms given atime-serie into an grid entropy image for input representation 
+%         in ConvNET
+%
+%               X...............time series
+%               n...............degree vector (first :R, second: G, third :B)
+%               level...........size of grid matrix (level x level)
+%               showP...........show image option (1 show, 0 not)
+%
+%
+%              Authored by , Apdullah Yayık 2018
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %USAGE
 % X=rand(1,1000)
 % im=tstbi(X, [1,2,3], 100, 1)
-%
-%AUTHOR
-% Apdullah Yayık, apdullahyayik@gmail.com
 
 for i=1:3
     im(:,:,i)= entropyM(X, 'grid', n(i), level, 0);
