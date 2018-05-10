62094761a92ee7493f4204511b8ca9180af56c27
Apdullah <apdullahyayik@gmail.com>
2018-05-10 13:20:01 +0300

    Update TimeSeries-to-be-image.m
    code is function, now

--- TimeSeries-to-be-image.m
+++ TimeSeries-to-be-image.m
@@ -1,9 +1,21 @@
-close all,clear all, clc
-a=rand(1,1000);
-R= entropyM2(a, 'grid', 1, 100, 0);
-G= entropyM2(a, 'grid', 2, 100, 0);
-B= entropyM2(a, 'grid', 3, 100, 0);
-im(:,:,1)=R;
-im(:,:,2)=G;
-im(:,:,3)=B;
-imshow(im)
+function im=tstbi(X, n, level, showP)
+%TIME SERIES-TO-BE-AN-IMAGE
+% X...............time series
+% n...............degree vector (first :R, second: G, third :B)
+% level...........size of grid matrix (level x level)
+% showP...........show image option (1 show, 0 not)
+%
+%USAGE
+% X=rand(1,1000)
+% im=tstbi(X, [1,2,3], 100, 1)
+%
+%AUTHOR
+% Apdullah Yayık, apdullahyayik@gmail.com
+
+for i=1:3
+    im(:,:,i)= entropyM(X, 'grid', n(i), level, 0);
+end
+    if showP==1
+        imshow(im)
+    end
+end
